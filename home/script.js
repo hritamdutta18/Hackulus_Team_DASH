1a5e29d50db55b4d65c5519b3094075c195ef9d4
Hritam Dutta <hritamloyola10@gmail.com>
2021-04-09 18:57:10 +0530

    Few changes

--- home/script.js
+++ home/script.js
@@ -1,12 +1,8 @@
 function logo_changer()
 {
-<<<<<<< HEAD
   document.getElementById('logo').src="../images/logo changer.gif";
-=======
-document.getElementById('logo').src="../images/logo changer0.gif";
->>>>>>> 59648df15ef5c380dc04a1c22d9c8a4fd3f4814e
 }
 function logo_changer2()
 {
-  document.getElementById('logo').src="../images/dog logo0.png";
+  document.getElementById('logo').src="../images/Dog logo.png";
 }
